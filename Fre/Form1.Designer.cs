b9b3f2e85f625143d8ccb9531e210db620a644fd
AsrielMao <maomichael37@gmail.com>
2020-02-10 20:16:38 +0800

    Upload Subject

---
+++ Fre/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace WordFrequencyStatistician
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_search = new System.Windows.Forms.Button();
+            this.txt_word = new System.Windows.Forms.TextBox();
+            this.txt_article = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label_result = new System.Windows.Forms.Label();
+            this.btn_statistics = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_search
+            // 
+            this.btn_search.Location = new System.Drawing.Point(965, 31);
+            this.btn_search.Name = "btn_search";
+            this.btn_search.Size = new System.Drawing.Size(75, 25);
+            this.btn_search.TabIndex = 0;
+            this.btn_search.Text = "搜索";
+            this.btn_search.UseVisualStyleBackColor = true;
+            this.btn_search.Click += new System.EventHandler(this.btn_search_Click);
+            // 
+            // txt_word
+            // 
+            this.txt_word.Location = new System.Drawing.Point(812, 31);
+            this.txt_word.Name = "txt_word";
+            this.txt_word.Size = new System.Drawing.Size(145, 25);
+            this.txt_word.TabIndex = 1;
+            // 
+            // txt_article
+            // 
+            this.txt_article.Location = new System.Drawing.Point(28, 33);
+            this.txt_article.Multiline = true;
+            this.txt_article.Name = "txt_article";
+            this.txt_article.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txt_article.Size = new System.Drawing.Size(528, 405);
+            this.txt_article.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 10F);
+            this.label1.Location = new System.Drawing.Point(677, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "搜索单个单词:";
+            // 
+            // label_result
+            // 
+            this.label_result.AutoSize = true;
+            this.label_result.Location = new System.Drawing.Point(593, 75);
+            this.label_result.Name = "label_result";
+            this.label_result.Size = new System.Drawing.Size(97, 15);
+            this.label_result.TabIndex = 4;
+            this.label_result.Text = "（计算结果）";
+            // 
+            // btn_statistics
+            // 
+            this.btn_statistics.Location = new System.Drawing.Point(596, 31);
+            this.btn_statistics.Name = "btn_statistics";
+            this.btn_statistics.Size = new System.Drawing.Size(75, 25);
+            this.btn_statistics.TabIndex = 5;
+            this.btn_statistics.Text = "统计";
+            this.btn_statistics.UseVisualStyleBackColor = true;
+            this.btn_statistics.Click += new System.EventHandler(this.btn_statistics_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(596, 104);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(444, 334);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "单词";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 170;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "次数";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 60;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "频率";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 60;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1072, 457);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_statistics);
+            this.Controls.Add(this.label_result);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_article);
+            this.Controls.Add(this.txt_word);
+            this.Controls.Add(this.btn_search);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_search;
+        private System.Windows.Forms.TextBox txt_word;
+        private System.Windows.Forms.TextBox txt_article;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label_result;
+        private System.Windows.Forms.Button btn_statistics;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+    }
+}
+
---
+++ Fre/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WordFrequencyStatistician
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_search_Click(object sender, EventArgs e)
+        {
+            if (txt_word.Text.Trim().Length == 0 || dataGridView1.RowCount - 1 < 1)
+            {
+                return;
+            }
+            //清空当前选择
+            dataGridView1.ClearSelection();
+
+            //查找
+            string word = txt_word.Text.Trim();
+            bool founded = false;
+            int rowCount = dataGridView1.RowCount - 1;
+            for (int i = 0; i < rowCount; i++)
+            {
+                if(word == dataGridView1.Rows[i].Cells[0].Value.ToString())
+                {
+                    dataGridView1.FirstDisplayedScrollingRowIndex = i;
+                    dataGridView1.Rows[i].Selected = true;
+                    founded = true;
+                    break;
+                }
+            }
+
+            if (!founded)
+            {
+                MessageBox.Show("当前文章不存在这个词！");
+            }
+        }
+
+        private void btn_statistics_Click(object sender, EventArgs e)
+        {
+            if (txt_article.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("请输入文本！");
+                return;
+            }
+
+            //清空列表内原有的内容
+            dataGridView1.Rows.Clear();
+
+            //处理输入的文章，将标点符号去掉，然后以空格分隔文章为单词组存入数组words中
+            string article = txt_article.Text.ToLower();
+
+            article = Regex.Replace(article, @"\W+", " ");
+            article = Regex.Replace(article, "\r\n", " ");
+            article = article.Trim();
+
+            string[] words = Regex.Split(article, " ");
+
+            //对words中的每一个词，查看是否已经存在于列表中，并计数
+            int word_total = 0;
+            foreach(string word in words)
+            {
+                word_total++;
+                int rc = dataGridView1.RowCount - 1;
+                bool had_added = false;
+                for(int i = 0; i < rc; i++)
+                {
+                    if(word == dataGridView1.Rows[i].Cells[0].Value.ToString())
+                    {
+                        int value = Convert.ToInt32(dataGridView1.Rows[i].Cells[1].Value);
+                        dataGridView1.Rows[i].Cells[1].Value = value + 1;
+                        had_added = true;
+                        break;
+                    }
+                }
+                if (had_added)
+                {
+                    continue;
+                }
+
+                int index = dataGridView1.Rows.Add();
+                dataGridView1.Rows[index].Cells[0].Value = word;
+                dataGridView1.Rows[index].Cells[1].Value = 1;
+                
+            }
+
+            //计算频率
+            int rowCount = dataGridView1.RowCount - 1;
+            for(int i = 0; i < rowCount; i++)
+            {
+                double count = Convert.ToDouble(dataGridView1.Rows[i].Cells[1].Value);
+                dataGridView1.Rows[i].Cells[2].Value = (count / word_total * 100).ToString("0.00") + "%";
+            }
+
+            //显示总词数
+            label_result.Text = "本篇文章共有单词 " + word_total + " 个。"; ;
+        }
+    }
+}
